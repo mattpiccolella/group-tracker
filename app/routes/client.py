66d9b9780f36da8d11cf6049fcef9e19b51ca576
Matt Piccolella <mjp2220@columbia.edu>
2015-11-03 04:50:42 +0000

    Getting admin nav working

--- app/routes/client.py
+++ app/routes/client.py
@@ -8,9 +8,13 @@ from app.utils import hash_pass
 
 client = Blueprint('client', __name__)
 
+# Landing Page
+
 @client.route('/', methods=['GET'])
 def index():
     return render_template('client/index.html')
+
+# Logged In View
 
 @client.route('/home', methods=['GET'])
 @redirect_for_no_auth
@@ -23,6 +27,22 @@ def home():
   number_of_members = members.count()
   return render_template('client/home.html', **locals())
 
+@client.route('/events', methods=['GET'])
+@redirect_for_no_auth
+def events():
+  organization = Organization.objects(id=session["organization_id"]).first()
+  events = Event.objects(organization=organization)
+  return render_template('client/events.html', events=events)
+
+@client.route('/members', methods=['GET'])
+@redirect_for_no_auth
+def members():
+  organization = Organization.objects(id=session["organization_id"]).first()
+  members = Member.objects(organization=organization)
+  return render_template('client/members.html', members=members)
+
+# Authentication
+
 @client.route('/login', methods=['GET','POST'])
 def login():
     form = OrganizationLoginForm(request.form)
@@ -34,7 +54,7 @@ def login():
             session["email"] = users[0].email
             session["organization_id"] = str(users[0].id)
             return redirect(url_for(".home", organization_id=str(users[0].id)))
-    return render_template('organizations/login.html', form=form)
+    return render_template('client/login.html', form=form)
 
 @client.route('/logout', methods=['GET','POST'])
 def logout():
